de7d8f55998259d5616d7f95cdf1d2d4455eedd5
cmorgantywls <cmorgan@tywlsbronx.org>
2019-03-22 17:37:23 -0400

    adding in code challenge front page
    because I should’ve started in atom.

---
+++ p5/homepage/functionAndClass.js
@@ -0,0 +1,97 @@
+
+class BubBub{
+ constructor(x,y){
+   this.x = x;
+   this.y = y;
+   this.ySpeed = random(-2,2);
+   this.xSpeed = random(-2,2);
+   this.col = color(255, 100);
+   this.diameter = 100
+ }
+
+  display(){
+    stroke(255);
+    fill(this.col);
+    strokeWeight(3);
+    ellipse(this.x,this.y,this.diameter)
+    // scribble.scribbleEllipse(this.x, this.y, this.diameter,this.diameter);
+  }
+
+  move(){
+      this.x = this.x + this.xSpeed
+    this.y = this.y + this.ySpeed
+
+    if (this.x > width || this.x < 0){
+      this.xSpeed=this.xSpeed*-1
+    }
+
+    if (this.y > height || this.y < 0){
+      this.ySpeed=this.ySpeed*-1
+    }
+  }
+
+  bubClick(){
+   let bubDist = dist(mouseX,mouseY,this.x,this.y)
+
+   if(bubDist<this.diameter/2){
+    console.log("clicky")
+   }
+  }
+
+}
+
+class interestingBub extends BubBub{
+ constructor(x,y,ySpeed,xSpeed,col,diameter){
+   super(x,y,ySpeed,xSpeed,col,diameter)
+   this.col = color(255,0,0,100);
+ }
+
+  resumeText(bubNum){
+    if (bubNum==1){
+    fill(0)
+    textFont("Economica")
+    textSize(16)
+    text("a resumé \n thing", this.x-20,this.y)
+    }
+  }
+
+  funText(bubNum){
+    if (bubNum==2){
+      fill(0)
+    textFont("Economica")
+    textSize(16)
+     text("a fun \n thing", this.x-20,this.y)
+    }
+  }
+
+  usefulText(bubNum){
+   if (bubNum==3){
+     fill(0)
+    textFont("Economica")
+    textSize(16)
+    text("a useful \n thing", this.x-20,this.y)
+   }
+  }
+
+  resumeClick(bubNum){
+    let bubDist = dist(mouseX,mouseY,this.x,this.y)
+   if (bubNum==1 && bubDist<this.diameter){
+    console.log("resume click")
+    }
+  }
+
+  funClick(bubNum){
+    let bubDist = dist(mouseX,mouseY,this.x,this.y)
+    if (bubNum==2 && bubDist<this.diameter){
+    console.log("fun click")
+    }
+  }
+
+  usefulClick(bubNum){
+    let bubDist = dist(mouseX,mouseY,this.x,this.y)
+    if (bubNum==3 && bubDist<this.diameter){
+    console.log("useful click")
+    }
+
+  }
+}
---
+++ p5/homepage/sketch.js
@@ -0,0 +1,93 @@
+//main page
+//name, etc
+//floatie bubs that bounce around - three of an
+//extended class that inherits stuff from BubBub
+//floaties should bounce on side of screen, interact on hover/click
+//if you click -> bubbles disappear or multiply???
+//if you click the 3, redirect to next sketch
+//3 other sketches:
+//resume (static)
+//tomagotchi (fun object)
+//math program??? (useful IDK)
+
+//gonna push some BubBubs in here.
+let floaties = []
+let multiBubs = []
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  //create new scribble object to use l8r
+
+
+    noStroke();
+  for (var i = 0; i < 10; i++) {
+    //makes sure to generate 10 bubbles
+    var x = random(200,400);
+    //sets a random location for x and y
+    var y = random(20,180);
+
+    if (i%3 == 0 && i != 0){
+      floaties[i] = new interestingBub(x,y)
+    }
+    else{
+    floaties[i] = new BubBub(x,y)
+    }
+    //adds a new bubble which is an OBJECT into my ARRAY.
+  }
+
+
+}
+
+function draw() {
+  background(220);
+  let bubNum=0
+
+  //add motion to floaty bubs
+   for (var i = 0; i < floaties.length; i++) {
+    floaties[i].move();
+    floaties[i].display();
+
+     if(i%3==0 && i != 0){
+       bubNum++
+      floaties[i].resumeText(bubNum)
+       floaties[i].funText(bubNum)
+       floaties[i].usefulText(bubNum)
+       //console.log(bubNum)
+     }
+
+   }
+
+  for(var z = 0;z<multiBubs.length;z++){
+    multiBubs[z].move()
+    multiBubs[z].display()
+  }
+
+  textFont("Megrim")
+  textSize(36)
+  fill(0);
+  text("Courtney Morgan", width/4,height/2)
+
+  textFont("Economica")
+  textSize(18)
+  text("Don't miss.", width/4,height/2+25)
+
+}
+
+function mouseClicked(){
+  let bubNum2 = 0
+
+  for (var i = 0; i < floaties.length; i++) {
+
+    if(i%3==0 && i != 0){
+       bubNum2++
+      floaties[i].resumeClick(bubNum2)
+       floaties[i].funClick(bubNum2)
+       floaties[i].usefulClick(bubNum2)
+       //console.log(bubNum)
+     }
+    else{
+      floaties[i].bubClick();
+      multiBubs.push(new BubBub(random(200,400),random(200,400)));
+    }
+   }
+}
