af70427f0d91697e1ddbb38c9b5f0f5bf65fcf32
cmorgantywls <cmorgan@tywlsbronx.org>
2019-04-10 11:17:23 -0400

    making HTML practice for a teacher training

---
+++ p5/teacherHTMLpractice/testing.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(400, 400);
+  console.log("javascript sketch linked!")
+  console.log("great use of the inspector :)")
+}
+
+function draw() {
+  background(220);
+}
